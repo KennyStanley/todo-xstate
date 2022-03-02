30a6876cf14be7f4cc7134332ce622171257d55b
nextstax-admin <nextstax@gmail.com>
2022-03-01 21:03:29 -0600

    feat: replaced css module with tailwindcss

---
+++ src/components/Content.tsx
@@ -0,0 +1,7 @@
+export default function Content() {
+  return (
+    <h2 className="text-2xl max-w-lg mx-auto font-mono">
+      A simple Next.js application with TypeScript and Tailwind CSS.
+    </h2>
+  )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <h1 className="text-7xl leading-normal bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 font-extrabold">
+      Next.js Project Template
+    </h1>
+  )
+}
